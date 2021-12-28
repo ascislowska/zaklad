7c5a859bd63d9a147d3472e874f741260124b879
Anna <anna.b.scislowska@gmail.com>
2021-12-29 00:56:47 +0100

    active menu, video background in 'visit us' section

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-$(document).ready(function(){
- $('.header').height($(window).height());
- 
-})
+ let galleryImg = document.querySelectorAll('.gallery img');
 
-$(".navbar a").click(function(){
-  $("body,html").animate({
-   scrollTop:$("#" + $(this).data('value')).offset().top
-  },1000)
- })
 
-$(".header button").click(function(){
-  $("body,html").animate({
-   scrollTop:$("#" + $(this).data('value')).offset().top
-  },1000)
- })
+// gallery animation while scrolled in //
+ function debounce(func, wait = 20, immediate = true) {
+   var timeout;
+   return function() {
+     var context = this, args = arguments;
+     var later = function() {
+       timeout = null;
+       if (!immediate) func.apply(context, args);
+     };
+     var callNow = immediate && !timeout;
+     clearTimeout(timeout);
+     timeout = setTimeout(later, wait);
+     if (callNow) func.apply(context, args);
+   };
+ };
+
+ function checkSlide() {
+   galleryImg.forEach(img => {
+     const slideInAt = (window.scrollY + window.innerHeight) - img.height / 2;
+     const imageBottom = img.offsetTop + img.height;
+     const isHalfShown = slideInAt > img.offsetTop;
+     const isNotScrolledPast = window.scrollY < imageBottom;
+     if (isHalfShown && isNotScrolledPast) {
+       img.classList.add('img-animated');
+     } else {
+       img.classList.remove('img-animated');
+     }
+   });
+ }
+
+ window.addEventListener('scroll', debounce(checkSlide));
